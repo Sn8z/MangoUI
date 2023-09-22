7ffa7aff31870af594037f054d98252bce721308
Marcus Sneitz <marcus.sneitz@gmail.com>
2023-09-22 09:57:22 +0200

    Removed stuff that is out of scope

--- General/hide.lua
+++ General/hide.lua
@@ -5,4 +5,15 @@ function mUI:HideStuff()
 	--BagsBar:Hide()
 end
 
+function mUI:HideTalkingHead()
+	-- TODO: Check db for setting
+	if true then
+		hooksecurefunc(TalkingHeadFrame, "PlayCurrent", function()
+			TalkingHeadFrame:Hide()
+		end)
+	end
+end
+
+mUI:HideTalkingHead()
+
 mUI:HideStuff()
