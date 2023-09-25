c93e947e04ccdc0256360df32d321a9da69fa191
Marcus Sneitz <marcus.sneitz@gmail.com>
2023-09-25 09:53:30 +0200

    Yet again added the media back..

---
+++ media.lua
@@ -0,0 +1,22 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- Inject Media
+-- Fonts (.ttf/.otf)
+LSM:Register(LSM.MediaType.FONT, "Manrope Regular",
+	[[Interface\Addons\MangoUI\Media\Fonts\Manrope\Manrope-Regular.ttf]])
+LSM:Register(LSM.MediaType.FONT, "Manrope Bold",
+	[[Interface\Addons\MangoUI\Media\Fonts\Manrope\Manrope-Bold.ttf]])
+LSM:Register(LSM.MediaType.FONT, "Manrope Light",
+	[[Interface\Addons\MangoUI\Media\Fonts\Manrope\Manrope-Light.ttf]])
+
+-- Statusbars (.tga/.blp)
+LSM:Register(LSM.MediaType.STATUSBAR, "Xulnif", [[Interface\Addons\MangoUI\Media\Statusbars\xulnif.tga]])
+LSM:Register(LSM.MediaType.STATUSBAR, "Sn8z", [[Interface\Addons\MangoUI\Media\Statusbars\sn8z.tga]])
+LSM:Register(LSM.MediaType.STATUSBAR, "Tim", [[Interface\Addons\MangoUI\Media\Statusbars\tim.tga]])
+LSM:Register(LSM.MediaType.STATUSBAR, "True", [[Interface\Addons\MangoUI\Media\Statusbars\true.tga]])
+LSM:Register(LSM.MediaType.STATUSBAR, "Patchouli", [[Interface\Addons\MangoUI\Media\Statusbars\patchouli.tga]])
+LSM:Register(LSM.MediaType.STATUSBAR, "Mango", [[Interface\Addons\MangoUI\Media\Statusbars\mango.tga]])
+
+-- Borders
+
+-- Sounds
