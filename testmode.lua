c277df69de36092074bb43c704c4153a91aa049f
Marcus Sneitz <marcus.sneitz@gmail.com>
2023-09-27 20:30:57 +0200

    added some initial testmode frames

---
+++ testmode.lua
@@ -0,0 +1,39 @@
+local _, mUI = ...
+-- Thanks to Phanx for this solution
+
+local frames = {
+	"oUF_MangoBoss1",
+	"oUF_MangoBoss2",
+	"oUF_MangoBoss3",
+	"oUF_MangoBoss4",
+	"oUF_MangoBoss5",
+	"oUF_MangoPrimaryPlayer",
+	"oUF_MangoPrimaryTarget",
+	"oUF_MangoPrimaryFocus",
+	"oUF_MangoSecondaryPet",
+	"oUF_MangoSecondaryTargetTarget"
+}
+
+local function toggle(f)
+	if f.__realunit then
+		f:SetAttribute("unit", f.__realunit)
+		f.unit = f.__realunit
+		f.__realunit = nil
+		f:Hide()
+	else
+		f.__realunit = f:GetAttribute("unit") or f.unit
+		f:SetAttribute("unit", "player")
+		f.unit = "player"
+		f:Show()
+	end
+end
+
+function mUI:ToggleFrames()
+	for i = 1, #frames do
+		toggle(_G[frames[i]])
+	end
+end
+
+-- Register slash command
+SLASH_MANGOTEST1, SLASH_MANGOTEST2 = "/mtest",  "/mangotest"
+SlashCmdList["MANGOTEST"] = mUI.ToggleFrames
