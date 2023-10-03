8cd30eef1ff0a7d10606241700f869471581223f
Marcus Sneitz <marcus.sneitz@gmail.com>
2023-10-03 22:45:58 +0200

    updated some stagger stuff

--- Components/stagger.lua
+++ Components/stagger.lua
@@ -7,19 +7,19 @@ function mUI:CreateStaggerBar(self)
 	local _, _, classIndex = UnitClass('player')
 	if classIndex ~= 10 then return end -- Check for Monk
 	local Stagger = CreateFrame('StatusBar', nil, self)
-	if mUI.db.player.classpower.detach then
-		Stagger:SetSize(mUI.db.player.classpower.width, mUI.db.player.classpower.height)
-		Stagger:SetPoint('LEFT', UIParent, 'CENTER', mUI.db.player.classpower.x, mUI.db.player.classpower.y)
+	if mUI.profile.player.classpower.detach then
+		Stagger:SetSize(mUI.profile.player.classpower.width, mUI.profile.player.classpower.height)
+		Stagger:SetPoint('LEFT', UIParent, 'CENTER', mUI.profile.player.classpower.x, mUI.profile.player.classpower.y)
 	else
-		Stagger:SetHeight(mUI.db.player.classpower.height)
+		Stagger:SetHeight(mUI.profile.player.classpower.height)
 		Stagger:SetPoint('BOTTOMLEFT', self.Health, 'TOPLEFT', 0, 2)
 		Stagger:SetPoint('BOTTOMRIGHT', self.Health, 'TOPRIGHT', 0, 2)
 	end
-	Stagger:SetStatusBarTexture(LSM:Fetch("statusbar", mUI.db.settings.texture))
+	Stagger:SetStatusBarTexture(LSM:Fetch("statusbar", mUI.profile.settings.texture))
 	mUI:CreateBorder(Stagger)
-	local sBackground = Stagger:CreateTexture(nil, "BACKGROUND")
-	sBackground:SetAllPoints(Stagger)
-	sBackground:SetColorTexture(0.15, 0.15, 0.15, 1)
+	local StaggerBackground = Stagger:CreateTexture(nil, "BACKGROUND")
+	StaggerBackground:SetAllPoints(Stagger)
+	StaggerBackground:SetColorTexture(0.1, 0.1, 0.1, 1)
+	Stagger.bg = StaggerBackground
 	self.Stagger = Stagger
-	self.Stagger.db = sBackground
 end
