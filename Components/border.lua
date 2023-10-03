062b289646a1f52b0df9829035f3fbc95b4ab313
Marcus Sneitz <marcus.sneitz@gmail.com>
2023-10-03 22:26:35 +0200

    added edgefile for border

--- Components/border.lua
+++ Components/border.lua
@@ -1,5 +1,7 @@
 local _, mUI = ...
 
+local UnitIsUnit, UnitExists = UnitIsUnit, UnitExists
+
 local function updateBorder(self)
 	if UnitExists("target") and UnitIsUnit(self.unit, "target") then
 		self.Border:SetBackdropBorderColor(1, 1, 1, 1)
@@ -10,13 +12,16 @@ end
 
 function mUI:CreateBorder(self, showTargeted)
 	showTargeted = showTargeted or false
-	local size = 2
+	local size = 1 -- TODO: load this from profile
 	local border = CreateFrame('Frame', nil, self, "BackdropTemplate")
 	border:SetPoint("TOPLEFT", self, "TOPLEFT", -size, size)
 	border:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", size, -size)
 	border:SetFrameStrata(self:GetFrameStrata())
 	border:SetFrameLevel(self:GetFrameLevel())
-	border:SetBackdrop({ edgeFile = "Interface\\Buttons\\WHITE8x8", edgeSize = size })
+	border:SetBackdrop({
+		edgeFile = [[Interface\AddOns\MangoUI\Media\border.tga]],
+		edgeSize = size
+	})
 	border:SetBackdropColor(0, 0, 0, 0)
 	border:SetBackdropBorderColor(0, 0, 0, 1)
 	self.Border = border
