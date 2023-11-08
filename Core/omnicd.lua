f7604bbd4e73fd150bbdb996fe74476f5b511912
Marcus Sneitz <marcus.sneitz@gmail.com>
2023-11-08 15:37:08 +0100

    added callback to avoid LUA error

--- Core/omnicd.lua
+++ Core/omnicd.lua
@@ -1,12 +1,16 @@
 local _, mUI = ...
 
+local function ToggleFrames()
+	print("Not yet implemented")
+end
+
 local f = CreateFrame("frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, addon)
 	if addon == "MangoUI" or addon == "OmniCD" then
 		local func = OmniCD and OmniCD.AddUnitFrameData
 		if func then
-			func("MangoUI", "oUF_MangoPartyUnitButton", "unit", 1, 5)
+			func("MangoUI", "oUF_MangoPartyUnitButton", "unit", 1, ToggleFrames, 5)
 		end
 	end
 end)
