17be03742f1bbcd249bf9617b9d341a60092a65e
Marcus Sneitz <marcus.sneitz@gmail.com>
2023-11-08 16:01:48 +0100

    Update omnicd.lua

--- Core/omnicd.lua
+++ Core/omnicd.lua
@@ -1,16 +1,16 @@
-local _, mUI = ...
+local addonName, mUI = ...
 
-local function ToggleFrames()
+local function ToggleTestFrames()
 	print("Not yet implemented")
 end
 
 local f = CreateFrame("frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, addon)
-	if addon == "MangoUI" or addon == "OmniCD" then
+	if addon == addonName or addon == "OmniCD" then
 		local func = OmniCD and OmniCD.AddUnitFrameData
 		if func then
-			func("MangoUI", "oUF_MangoPartyUnitButton", "unit", 1, ToggleFrames, 5)
+			func("MangoUI", "oUF_MangoPartyUnitButton", "unit", 1, ToggleTestFrames, 5)
 		end
 	end
 end)
