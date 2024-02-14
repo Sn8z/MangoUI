e7eea360c15ffb6c29001e9e6673dfea04beb214
Marcus Sneitz <marcus.sneitz@gmail.com>
2024-02-14 09:04:50 +0100

    tidying up some old stuff

--- Options/dropdown.lua
+++ Options/dropdown.lua
@@ -91,7 +91,6 @@ function mUI:CreateDropdown(labelText, text, values, frame, callback, isTexture)
 
 	for _, v in pairs(values) do
 		CreateDropdownItem(v, function()
-			print(v)
 			callback(v)
 			ddBtn.label:SetText(v)
 			ddFrame:Hide()
@@ -107,30 +106,3 @@ function mUI:CreateDropdown(labelText, text, values, frame, callback, isTexture)
 
 	return ddBtn
 end
-
---function mUI:CreateDropdown(labelText, text, values, frame, callback)
---	local dropdown = CreateFrame("Frame", nil, frame, "UIDropDownMenuTemplate")
---	UIDropDownMenu_SetWidth(dropdown, 200)
---	UIDropDownMenu_SetText(dropdown, text)
---	UIDropDownMenu_Initialize(dropdown, function(self, level, _)
---		local info = UIDropDownMenu_CreateInfo()
---		for _, v in pairs(values) do
---			info.text, info.arg1 = v, v
---			info.func = function(self, arg1, arg2, checked)
---				UIDropDownMenu_SetText(dropdown, arg1)
---				callback(arg1)
---				CloseDropDownMenus()
---			end
---			UIDropDownMenu_AddButton(info, level)
---		end
---	end)
---
---	local label = dropdown:CreateFontString(nil, "OVERLAY")
---	label:SetPoint("BOTTOMLEFT", dropdown, "TOPLEFT", 20, 5)
---	label:SetJustifyH("LEFT")
---	label:SetFont(LSM:Fetch("font", "Ubuntu Medium"), 10, "THINOUTLINE")
---	label:SetText(labelText)
---	label:SetTextColor(1, 1, 1, 1)
---
---	return dropdown
---end
